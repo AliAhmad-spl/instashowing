f804e5f694eb44a9a1c30b879ce7a1fab530256e
aliahmad66 <ali.ahmed@buzzinteractive.co>
2019-05-15 13:22:31 +0500

    stripe added

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,22 @@
-Rails.configuration.stripe = {
-  publishable_key: Rails.application.secrets.stripe_publishable_key,
-  secret_key:      Rails.application.secrets.stripe_secret_key
-}
+if Rails.env.development?
+  
+  Rails.configuration.stripe = {
+    :publishable_key => Rails.application.secrets['PUBLISHABLE_KEY'],
+    :secret_key      => Rails.application.secrets['SECRET_KEY']
+  
+  }
+  
+  Stripe.api_key = Rails.configuration.stripe[:secret_key]  
+  
+end
 
-Stripe.api_key = Rails.configuration.stripe[:secret_key]
+if Rails.env.production?
+  
+  Rails.configuration.stripe = {
+    :publishable_key => ENV['PUBLISHABLE_KEY'],
+    :secret_key      => ENV['SECRET_KEY']
+  }
+  
+  Stripe.api_key = Rails.configuration.stripe[:secret_key]
+
+end
